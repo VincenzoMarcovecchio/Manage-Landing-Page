e24274ab73b9e8edb238af3dec328a80fe540843
VIncenzo Marcovecchio <vinceoldmark@gmail.com>
2020-02-14 09:14:12 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+new Glider(document.querySelector(".glider"), {
+  slidesToShow: 2.5,
+  draggable: true,
+  dots: "#dots",
+  arrow: {
+    prev: ".glider-prev",
+    next: ".glidet-next"
+  }
+});
+
+const error = document.querySelector(".error");
+
+const email = document.querySelector(".email");
+
+function checkInput() {
+  const emailValue = email.value.trim();
+  if (emailValue === "") {
+    setErrorFor();
+    changePlaceHolder();
+  } else if (!isEmail(emailValue)) {
+    setErrorFor();
+    changePlaceHolder();
+  } else {
+    setSuccessFor();
+  }
+}
+
+function isEmail() {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
+
+function setErrorFor() {
+  error.style.display = "block";
+}
+function setSuccessFor() {
+  error.style.display = "Inherit";
+}
+function changePlaceHolder() {
+  email.placeholder = "email@example.com";
+  email.placeholder.style.color = "red";
+}
+
+document.querySelector(".form").addEventListener("submit", e => {
+  e.preventDefault();
+  checkInput();
+});
+
+const hamburger = document.querySelector(".hamburger-icon");
+
+hamburger.addEventListener("click", function() {
+  hamburger.classList.toggle("hamburger-close");
+  showMenu();
+});
+
+function showMenu() {
+  const menu = document.querySelector(".menu");
+
+  menu.classList.toggle("show");
+}
